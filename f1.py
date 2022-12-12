32741089ba28eddf7c1eeba6dfe042a151e258dd
Your Name <you@example.com>
2022-12-12 15:25:51 +0400

    Initial commit

---
+++ f1.py
@@ -0,0 +1,6 @@
+def sum2n(x,y):
+    x=int(x)
+    y=int(y)
+    a = x+y
+    return a
+
---
+++ test.py
@@ -0,0 +1,17 @@
+from f1 import sum2n
+
+def test1():
+    test = sum2n(1,2)
+    if test == 2:
+        print('test passed')
+    else:
+        print('test failed')
+
+def test2():
+    test = sum2n('2', '3')
+    if type(test) != int:
+        print('test failed')
+    else:
+        print('test passed')
+
+test2()
